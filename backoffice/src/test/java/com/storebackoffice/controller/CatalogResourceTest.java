69727bda0e83c9d2621cc1992f6f01a340113eb1
flaviano-faria <flsilv89@yahoo.com.br>
2026-03-17 20:48:35 -0300

    Updating unit tests from CatalogResource

--- backoffice/src/test/java/com/storebackoffice/controller/CatalogResourceTest.java
+++ backoffice/src/test/java/com/storebackoffice/controller/CatalogResourceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 
 @QuarkusTest
 class CatalogResourceTest {
@@ -20,5 +21,25 @@ class CatalogResourceTest {
                 .body("categories.size()", greaterThanOrEqualTo(1))
                 .body("categories[0].name", notNullValue())
                 .body("categories[0].products", notNullValue());
+    }
+
+    @Test
+    void getCatalog_returnsExpectedCategoryNames() {
+        given()
+                .when().get("/catalog")
+                .then()
+                .statusCode(200)
+                .body("categories.name", hasItems("Electronics", "Clothing", "Home & Garden"));
+    }
+
+    @Test
+    void getCatalog_returnsProductsWithRequiredFields() {
+        given()
+                .when().get("/catalog")
+                .then()
+                .statusCode(200)
+                .body("categories[0].products[0].id", notNullValue())
+                .body("categories[0].products[0].name", notNullValue())
+                .body("categories[0].products[0].price", notNullValue());
     }
 }
